8f0ee76050e5a41ca60f161b04f3f1bb2350a6c0
Pandu Sastrowardoyo <1@decentri.city>
2023-05-25 10:18:48 +0700

    Update socialtokens.sh

--- socialtokens.sh
+++ socialtokens.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Check if near-cli is installed, if not then install it
+echo "Checking if near-cli is installed..."
+sleep 0.25
 if ! command -v near &> /dev/null
 then
     echo "near-cli could not be found, installing it now..."
     npm install -g near-cli
 fi
 
-# Get the account from the script parameters
+echo "Fetching NEAR account from the script parameters..."
+sleep 0.25
 ACCOUNT_ID=$1
 
 if [ -z "$ACCOUNT_ID" ]; then
@@ -15,50 +17,60 @@ if [ -z "$ACCOUNT_ID" ]; then
     exit 1
 fi
 
-# Check if FT repo exists
+echo "Checking if FT repo exists..."
+sleep 0.25
 if [ ! -d "FT" ]; then
-    # Clone the FT repository
+    echo "Cloning the FT repository..."
+    sleep 0.25
     git clone https://github.com/near-examples/FT.git
-
-    # Change directory to FT
     cd FT
 else
-    # If repo exists, just navigate to it
+    echo "FT repo exists, navigating to it..."
+    sleep 0.25
     cd FT
 fi
 
-# Sub-account creation
+echo "Creating sub-account..."
+sleep 0.25
 SUB_ACCOUNT=myriadsocial.$ACCOUNT_ID
 near create-account $SUB_ACCOUNT --masterAccount $ACCOUNT_ID --initialBalance 10
 
-# Build the fungible token contract
+echo "Building the fungible token contract..."
+sleep 0.25
 ./scripts/build.sh
 
-# Deploy the fungible token contract to the sub-account
+echo "Deploying the fungible token contract to the sub-account..."
+sleep 0.25
 near deploy --wasmFile res/fungible_token.wasm --accountId $SUB_ACCOUNT
 
-# Metadata for the fungible token
+echo "Preparing metadata for the fungible token..."
+sleep 0.25
 TOKEN_NAME="Token of $ACCOUNT_ID"
 TOKEN_SYMBOL=$(echo ${ACCOUNT_ID:0:5} | awk '{print toupper($0)}')
 TOTAL_SUPPLY="100000000"
 DECIMALS=8
 SPEC="ft-1.0.0"
 
-# Initialize the fungible token contract
+echo "Initializing the fungible token contract..."
+sleep 0.25
 near call $SUB_ACCOUNT new '{"owner_id": "'$SUB_ACCOUNT'", "total_supply": "'$TOTAL_SUPPLY'", "metadata": { "spec": "'$SPEC'", "name": "'$TOKEN_NAME'", "symbol": "'$TOKEN_SYMBOL'", "decimals": '$DECIMALS' }}' --accountId $SUB_ACCOUNT
 
 echo "Fungible Token '$TOKEN_NAME' has been deployed and initialized by sub-account '$SUB_ACCOUNT'"
 echo "Sub-account '$SUB_ACCOUNT' has been created under '$ACCOUNT_ID'"
+sleep 0.25
 
-# Assume 10 tokens are to be sent
+echo "Preparing to send tokens..."
+sleep 0.25
 TOKENS_TO_SEND=10
 RECIPIENT_ID="decentricity.testnet"
 
-# Transfer tokens from the main account to the recipient
-near call $SUB_ACCOUNT_ID ft_transfer '{"receiver_id": "'$RECIPIENT_ID'", "amount": "'$TOKENS_TO_SEND'"}' --accountId $NEAR_ACCOUNT_ID --amount 0.000000000000000000000001
+echo "Transferring tokens from the sub-account to the recipient..."
+sleep 0.25
+near call $SUB_ACCOUNT ft_transfer '{"receiver_id": "'$RECIPIENT_ID'", "amount": "'$TOKENS_TO_SEND'"}' --accountId $SUB_ACCOUNT --amount 0.000000000000000000000001
 
-echo "Sent $TOKENS_TO_SEND tokens to $RECIPIENT_ID from $SUB_ACCOUNT_ID"
+echo "Sent $TOKENS_TO_SEND tokens to $RECIPIENT_ID from $SUB_ACCOUNT"
+sleep 0.25
 
-
-# Move back to original directory
+echo "Moving back to original directory..."
+sleep 0.25
 cd ..
